3a3ac6314f35c6509bdf43c5fda09fa674270c5d
dmitryserafin <dmitryserafin@gmail.com>
2023-05-16 19:14:54 +0300

    add tables: ProductCategory and product

--- products/models.py
+++ products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    description = models.TextField(null=True,blank=True)
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=512)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    quantity = models.PositiveIntegerField (default=0)
+    image = models.ImageField(upload_to='products_images')
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.PROTECT)
+
+
